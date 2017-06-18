16e74af58a4d359778e33c20cd90d2f3ca7da2a9
Chuck Harmston <chuck@mozilla.com>
2017-06-18 14:43:20 -0600

    Adds selector to filter tasks.

--- src/containers/tasks/index.js
+++ src/containers/tasks/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { filterActions } from '../../actions/filters';
 import { taskStatus } from '../../actions/tasks';
 import Loading from '../../components/loading';
 import TaskList from '../../components/task-list';
+
+import filteredTasks from '../../selectors/filters';
+
 import './index.css';
 
 class Tasks extends Component {
@@ -14,7 +18,7 @@ class Tasks extends Component {
         {elem}
       </section>
     );
-  };
+  }
 
   render() {
     const { status, tasks } = this.props;
@@ -33,7 +37,12 @@ const mapStateToProps = state => ({
   repos: state.filters.available.repos,
   skills: state.filters.available.skills,
   status: state.tasks.status,
-  tasks: state.tasks.data
+  tasks: filteredTasks(state)
 });
 
-export default connect(mapStateToProps)(Tasks);
+const mapDispatchToProps = dispatch => ({
+  changeRepo: repo => dispatch(filterActions.changeRepoFilter(repo)),
+  changeSkill: repo => dispatch(filterActions.changeSkillFilter(repo))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tasks);
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -15,13 +15,12 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FILTERS_CHANGE_REPO:
-      return Object.assign({}, state, { repo: action.payload });
+      return Object.assign({}, state, { repo: action.slug });
 
     case actionTypes.FILTERS_CHANGE_SKILL:
-      return Object.assign({}, state, { skill: action.payload });
+      return Object.assign({}, state, { skill: action.slug });
 
     case actionTypes.FILTERS_SET_AVAILABLE:
-      console.log(action);
       const available = Object.assign(
         {},
         {
