346628140f95b7652dd05a9a775cc99aa10567d3
Chuck Harmston <chuck@mozilla.com>
2017-06-18 16:35:18 -0600

    Logo links to homepage.

--- src/containers/tasks/index.js
+++ src/containers/tasks/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import URI from 'urijs';
 
 import { filterActions } from '../../actions/filters';
 import { taskStatus } from '../../actions/tasks';
