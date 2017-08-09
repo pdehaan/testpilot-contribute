07758a46ab19d4c260556286d84786d51aa0ccc2
Chuck Harmston <chuck@mozilla.com>
2017-08-09 16:12:38 -0600

    Adds hrefs to footer links.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,18 +8,18 @@ export default class Footer extends Component {
       <footer className="footer">
         <nav>
           <ul>
-            <li className="footer--mozilla"><a href="">Mozilla</a></li>
-            <li><a href="">Legal</a></li>
-            <li><a href="">About Test Pilot</a></li>
-            <li><a href="">Privacy</a></li>
-            <li><a href="">Terms</a></li>
-            <li><a href="">Cookies</a></li>
+            <li className="footer--mozilla"><a href="https://www.mozilla.org/">Mozilla</a></li>
+            <li><a href="https://www.mozilla.org/about/legal/">Legal</a></li>
+            <li><a href="https://testpilot.firefox.com/about">About Test Pilot</a></li>
+            <li><a href="https://testpilot.firefox.com/privacy">Privacy</a></li>
+            <li><a href="https://testpilot.firefox.com/terms">Terms</a></li>
+            <li><a href="https://www.mozilla.org/privacy/websites/#cookies">Cookies</a></li>
           </ul>
         </nav>
         <nav>
           <ul>
-            <li className="footer--github"><a href="">GitHub</a></li>
-            <li className="footer--twitter"><a href="">Twitter</a></li>
+            <li className="footer--github"><a href="https://github.com/chuckharmston/testpilot-contribute">GitHub</a></li>
+            <li className="footer--twitter"><a href="https://twitter.com/fxtestpilot">Twitter</a></li>
           </ul>
         </nav>
       </footer>
